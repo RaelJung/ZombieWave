95b60318c3b8aaeaf717a4cd1ac76e62d4940f1d
Rael <allice2283006@gmail.com>
2026-04-28 17:56:29 +0900

    fix: Kotlin 런타임 jar에 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,4 +18,5 @@ dependencies {
 tasks.jar {
     from(sourceSets.main.get().resources)
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
